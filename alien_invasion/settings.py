35ffc7be79176681b6fa2c6e789eeb27cabdcb52
zyh6627 <zouyihan0227@gmail.com>
2026-03-10 19:35:05 +0800

    first upload

---
+++ alien_invasion/settings.py
@@ -0,0 +1,16 @@
+
+class Settings():
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+        self.ship_speed=6
+        self.ship_limit=3
+        self.bullet_speed=2.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed=100
+        self.alien_speed=1.0
+        self.fleet_drop_speed=10
+        self.fleet_direction=1
